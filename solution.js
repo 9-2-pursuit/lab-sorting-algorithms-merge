f4582b18ce434608456331e915039b711f42ab02
Wwright91 <wisdomwright-martin@pursuit.org>
2023-04-09 07:41:25 -0400

    All tests pass

--- solution.js
+++ solution.js
@@ -1,34 +1,92 @@
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
-const sortNumsA = (nums) => {return nums.sort((a, b) => a - b)};
+const sortNumsA = (nums) => {
+  return nums.sort((a, b) => a - b);
+};
 
 // sort numbers in descending order
-const sortNumsD = (nums) => {return nums.sort((a, b) => b - a)};
+const sortNumsD = (nums) => {
+  return sortNumsA(nums).reverse();
+};
 
 // sort words in ascending order case sensitive
-const sortWordsA = (words) => {return words.sort((a, b) => a > b ? 1 : -1)};
+const sortWordsA = (words) => {
+  return words.sort((a, b) => (a > b ? 1 : -1));
+};
 
 // sort words in descending order case insensitive
-const sortWordsD = (words) => {return words.sort((a, b) => a.toLowerCase() < b.toLowerCase() ? 1 : a.toLowerCase() > b.toLowerCase() ? -1 : 0)};
+const sortWordsD = (words) => {
+  return words.sort((a, b) =>
+    a.toLowerCase() < b.toLowerCase()
+      ? 1
+      : a.toLowerCase() > b.toLowerCase()
+      ? -1
+      : 0
+  );
+};
 
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = (products) => {return products.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0)};
+const sortProductNamesA = (products) => {
+  return products.sort((a, b) =>
+    a.name > b.name ? 1 : a.name < b.name ? -1 : 0
+  );
+};
 
 // sort products by price, ascending order
-const sortProductPriceA = (products) => {return products.sort((a, b) => a.price > b.price ? 1 : a.price < b.price ? -1 : 0)};
+const sortProductPriceA = (products) => {
+  return products.sort((a, b) => a.price - b.price);
+};
 
 // sort products by price, descending order
-const sortProductPriceD = (products) => {return products.sort((a, b) => a.price < b.price ? 1 : a.price > b.price ? -1 : 0)};
+const sortProductPriceD = (products) => {
+  return products.sort((a, b) => b.price - a.price);
+};
 
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = (products) => {
+  return products.sort((a, b) => {
+    if (a.price === b.price) {
+      return a.name > b.name ? 1 : -1;
+    }
+    return a.price - b.price;
+  });
+};
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
+const catArtSortDesginedByA = (art) => {
+  return art.sort((a, b) =>
+    a.designedBy > b.designedBy ? 1 : a.designedBy < b.designedBy ? -1 : 0
+  );
+};
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (art) => {
+  let sorted = true;
+
+  while (sorted) {
+    sorted = false;
+
+    for (let i = 0; i < art.length - 1; i++) {
+      let newPrice1 = Number(art[i].price);
+      let newPrice2 = Number(art[i + 1].price);
+
+      if (isNaN(newPrice1)) {
+        newPrice1 = Number(10 * art[i].price.slice(-1));
+      }
+      if (isNaN(newPrice2)) {
+        newPrice2 = Number(10 * art[i + 1].price.slice(-1));
+      }
+      if (newPrice1 > newPrice2) {
+        sorted = true;
+        let temp = art[i + 1];
+        art[i + 1] = art[i];
+        art[i] = temp;
+      }
+    }
+  }
+  return art;
+};
 
 // Create your own sort function
 // it should sort in ascending order
@@ -38,7 +96,23 @@ const catArtSortByPriceA = () => {};
 // or try to implement merge sort
 // or look up another common sort algorithm (i.e. quicksort, ) and try your own implementation
 // Bonus add another argument that would allow the function to be used for ascending or descending order
-const mySortFunction = () => {};
+
+function mySortFunction(arr, ascending = true) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mySortFunction(arr.slice(0, mid));
+  let right = mySortFunction(arr.slice(mid));
+  let sortedArr = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+  sortedArr.push(...left, ...right);
+  return ascending ? [...sortedArr] : [...sortedArr].reverse();
+}
 
 module.exports = {
   sortNumsA,
